eed60874e3ce860049c4d191cc690518e6c006d1
Graham <gmattox2@huskers.unl.edu>
2021-08-26 13:20:42 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Graham Mattox
+ *
+ * Date: 2020/08/26
+ *
+ * A simple hello program that prints my name in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Graham Mattox\n");
+
+  return 0;
+}
